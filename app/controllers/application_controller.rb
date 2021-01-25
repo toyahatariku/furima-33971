5e92176497eb8b0772c0aaae32c40045101992ae
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-25 12:41:41 +0900

    Basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'kuritahayato' && password == '123456'
+    end
+  end
 end
