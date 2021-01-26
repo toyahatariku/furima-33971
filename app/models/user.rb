2d3c2308f79300981384c2d35e826d4d64f113b0
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-26 18:00:25 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence:true, uniqueness: true
+
+  with_options presence: true do
+    validates :encrypted_password, :nickname, :first_name, :second_name, :first_name_kana, :second_name_kana, :birthday
+  end
+
 end
