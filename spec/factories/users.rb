754cff3d235c866489e2308d4043446a20157948
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-27 17:00:31 +0900

    FactroyBotでuser情報作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
-    email                 {"test@example"}
-    password              {"abc123"}
+    nickname              {Faker::Name.initials(number: 5)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     first_name            {"山田"}
     second_name           {"太郎"}
     first_name_kana       {"ヤマダ"}
     second_name_kana      {"タロウ"}
-    birthday              {"1930-01-01"}
+    birthday              {Faker::Date.backward}
   end
 end
