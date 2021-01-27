ad5e0bb52d89bbfa25e6e5a0867a628fd3a5b25e
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-27 12:15:47 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence:true, uniqueness: true
+# 英数字を含めるパスワード
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  with_options presence: true, format:  { with: PASSWORD_REGEX, message: '英数字をそれぞれ１文字以上含め６文字以上にしてください' } do
+    validates :password, :password_confirmation, presence:true
+  end
+# 全角文字のみの名前 
+  ZENKAKU_MOJI = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
+  with_options presence: true, format: { with: ZENKAKU_MOJI, message: '全角文字を使用してください' } do
+    validates :first_name, :second_name
+  end
+# カタカナのみの名前
+  KATAKANA_MOJI = /\A[ァ-ヶ]+\z/.freeze
+  with_options presence: true, format: { with: KATAKANA_MOJI, message: 'カタカナのみを使用してください' } do
+    validates :first_name_kana, :second_name_kana
+  end
 
+# 記入必須
   with_options presence: true do
-    validates :encrypted_password, :nickname, :first_name, :second_name, :first_name_kana, :second_name_kana, :birthday
+    validates :nickname, :birthday
   end
 
 end
