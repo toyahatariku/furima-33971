4629d2669097f970aef6b340248929ffbcd3426a
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-28 16:50:40 +0900

    imageとのアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :imgae
+  
   with_options presence: true do
     validates :user, :name, :description, :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price
+  end
+
 end
