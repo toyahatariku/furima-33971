a924c8b0bb914103b20f8e2e26ebbb823d7c491f
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-28 19:15:15 +0900

    ID番号ズレの修正

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -1,10 +1,10 @@
 class Days_to_ship < ActiveHash::Base
 #発送までの日数
   self.data = [
-    { id: 0, name: '---' },
-    { id: 1, name: '1〜2日で発送' },
-    { id: 2, name: '2〜3日で発送' },
-    { id: 3, name: '4〜7日で発送' },
+    { id: 1, name: '---' },
+    { id: 2, name: '1〜2日で発送' },
+    { id: 3, name: '2〜3日で発送' },
+    { id: 4, name: '4〜7日で発送' },
   ]
 
   include ActiveHash::Associations
--- app/models/shipping_burden.rb
+++ app/models/shipping_burden.rb
@@ -1,9 +1,9 @@
 class Shipping_burden < ActiveHash::Base
 #配送料の負担
   self.data = [
-    { id: 0, name: '---' },
-    { id: 1, name: '着払い（購入者負担）' },
-    { id: 2, name: '送料込み（出品者負担）' },
+    { id: 1, name: '---' },
+    { id: 2, name: '着払い（購入者負担）' },
+    { id: 3, name: '送料込み（出品者負担）' },
   ]
 
   include ActiveHash::Associations
