db086bbe85bafc2ea6cec91eff805e51a8003fc0
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-28 19:10:59 +0900

    バリデーションの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :days_to_ship, :prefecture, :shipping_burden
 
+  validates :title, :text, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :genre_id, numericality: { other_than: 1 } 
+
 end
