ff5b7b83fa2faa615c5e4292f4ef5313dede3fe9
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-28 16:57:56 +0900

    ストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item.save(item_params)
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
