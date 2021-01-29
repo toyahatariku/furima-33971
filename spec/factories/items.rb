613064ed578af554de463b86291d68e2f7ce5e59
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-29 19:15:39 +0900

    FactoryBotのitem登録

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { Faker::Games::Pokemon.name }
+    description         { Faker::Games::Pokemon.move }
+    category_id         {2}
+    condition_id        {2}
+    shipping_burden_id  {2}
+    prefecture_id       {2}
+    days_to_ship_id     {2}
+    price               {10000}
   end
 end
