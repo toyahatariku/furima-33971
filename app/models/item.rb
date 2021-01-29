05cae2699c9642360514cfeb995934aeefd113ea
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-29 16:51:35 +0900

    値段制限のバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,12 @@ class Item < ApplicationRecord
   # 選択肢が"---"の時は保存できない
   validates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 } 
 
+  # priceの値段が300円以上・9,999,999円以下
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  # 記入必須
   with_options presence: true do
-    validates :user, :name, :description, :price, :image
+    validates :user, :name, :description, :price,:category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, numericality:, :image
   end
 
 end
