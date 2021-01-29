f65325eb6d7015d1dd9d33eb08e26718bad98ef7
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-29 11:26:53 +0900

    マイグレートの実行

--- db/migrate/20210129022224_create_items.rb
+++ db/migrate/20210129022224_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
-      
+      t.references :user                  ,null: false
+      t.string     :name                  ,null: false, default: ""
+      t.text       :description           ,null: false
+      t.integer    :category_id           ,null: false
+      t.integer    :condition_id          ,null: false
+      t.integer    :shipping_burden_id    ,null: false
+      t.integer    :prefecture_id         ,null: false
+      t.integer    :days_to_ship_id       ,null: false
+      t.integer    :price                 ,null: false
+  
       t.timestamps
     end
   end
