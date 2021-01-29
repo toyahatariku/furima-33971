130862b909a4944cff17043ef2a294da0d53adc7
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-29 17:02:56 +0900

    バリデーションのコメント修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   # 選択肢が"---"の時は保存できない
   validates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 } 
 
-  # priceの値段が300円以上・9,999,999円以下
+  # priceの数値が半角のみで300以上 9,999,999以下
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   # 記入必須
