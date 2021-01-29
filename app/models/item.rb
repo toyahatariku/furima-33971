45a28ce12dc63153d5b56f4dd8eeaa8c27b06e31
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-29 11:32:38 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :imgae
 
+  with_options presence: true do
+    validates :user, :name, :description, :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price
+  end
 
 end
