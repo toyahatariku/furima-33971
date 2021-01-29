0de48f9ee58ca707ba427c70a2d34333a4442171
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-29 11:35:35 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :shipping_burden_id
+  belongs_to :prefecture_id
+  belongs_to :days_to_ship_id
 
   belongs_to :user
   has_one_attached :imgae
