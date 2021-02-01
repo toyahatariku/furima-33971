c8a3b0d11491c617647423f6ee3acb734902c302
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-01 10:57:58 +0900

    全項目が埋まっていれば登録出来る

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '出品情報の登録' do
+    context '出品登録ができる場合' do
+      it '全ての項目が埋まっていると登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+  end
+
 end
