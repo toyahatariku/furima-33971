d66f2db2b6997b22ec0079328b3ac1c11bf2db13
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-01 11:38:58 +0900

    文字数バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+
+  # 商品名は40文字まで
+  validates :name, length: { maximum: 40 }
+
+  # 商品説明は1000文字まで
+  validates :description, length: { maximum: 1000 }
+
   # 選択肢が"---"の時は保存できない
   validates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 } 
 
