6b52933609472bbc58777a7763538a55928bf19c
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-01 14:41:28 +0900

    販売価格英数字を含むテストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,10 +10,6 @@ RSpec.describe Item, type: :model do
       it '全ての項目が埋まっていると登録できる' do
         expect(@item).to be_valid
       end
-      it '販売価格が半角数字だと登録できる' do
-        @item.price = '1000'
-        expect(@item).to be_valid
-      end
     end
 
     context '出品登録ができない場合' do
@@ -110,9 +106,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
-
       it '販売価格が全角数字だと登録できない' do
         @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price is not a number'
+      end
+      it '販売価格が半角英数混合だと登録できない' do
+        @item.price = '1000yen'
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price is not a number'
+      end
+      it '販売価格が英字だと登録できない' do
+        @item.price = 'thousand'
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price is not a number'
       end
