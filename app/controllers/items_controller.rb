735cd2e182c743da82704f25288c6f56f8295896
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-01 16:39:26 +0900

    indexアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
+  def index
+    @items = Item.all
+  end
+
   def new
     @item = Item.new
   end
