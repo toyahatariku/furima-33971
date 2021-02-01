39b6ba186425c0fc868febcdf247d62df42c8646
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-01 11:57:51 +0900

    販売価格が範囲外だと登録出来ないコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -121,9 +121,15 @@ RSpec.describe Item, type: :model do
 
       end
       it '販売価格が300円未満だと登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
 
       end
       it '販売価格が10,000,000円以上だと登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
 
       end
     end
