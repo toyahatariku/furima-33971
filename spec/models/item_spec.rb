3cb4d494fee945837bd31e0a6579aaac8cdc90e7
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-01 11:08:57 +0900

    商品が登録出来る場合の単体テストコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,8 +10,23 @@ RSpec.describe Item, type: :model do
       it '全ての項目が埋まっていると登録できる' do
         expect(@item).to be_valid
       end
+      it '商品名が40文字までなら登録できる' do
+        @item.name = "あ" * 40
+        expect(@item).to be_valid
+      end
+      it '商品の説明が1000文字までなら登録できる' do
+        @item.description = "あ" * 1000
+        expect(@item).to be_valid
+      end
+      it '値段設定が300円以上なら登録できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
+      it '値段設定が9999999円までなら登録出来る' do
+        @item.price = 9999999
+        expect(@item).to be_valid
+      end
     end
 
   end
-
 end
