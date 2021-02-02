c226773b10d45177e1687bf2218c9e66a4ae790f
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-02 17:11:48 +0900

    不要なuser_signed_in?の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,6 +43,6 @@ class ItemsController < ApplicationController
 
   def back_index
     @item = Item.find(params[:id])
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
