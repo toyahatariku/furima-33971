3775125865b8310b0b3e5029e2591eaacf81e857
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-03 19:30:58 +0900

    購入画面の表示の修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,23 @@
 class PurchasesController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
   end
 
   def create
+binding.pry
+    # @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
+
+  private
+
+  # def purchase_params
+  #   params.require(:purchase).permit(:price)
+  # end
 end
