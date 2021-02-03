acde23c2a627ee5421c9e47971d849f03bde3e08
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-03 18:06:29 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_104940) do
+ActiveRecord::Schema.define(version: 2021_02_03_085609) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_02_01_104940) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "purchase_id", null: false
+    t.string "postal_code", default: "", null: false
+    t.integer "prefecture_id", null: false
+    t.string "municipality", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "building_name", default: ""
+    t.string "phone_number", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchase_id"], name: "index_addresses_on_purchase_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
