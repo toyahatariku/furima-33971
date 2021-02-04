e60f982a9ebe39ba0407e82531bcf83717651f06
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-04 11:36:38 +0900

    FactoryBotの修正

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -10,10 +10,3 @@ FactoryBot.define do
     association :purchase
   end
 end
-# | purchase      | references | null: false, foreign_key: true | //購入履歴ID
-# | postal_code   | string     | null: false                    | //郵便番号
-# | prefecture_id | integer    | null: false                    | //都道府県
-# | municipality  | string     | null: false                    | //市区町村
-# | address       | string     | null: false                    | //番地
-# | building_name | string     |                                | //建物名
-# | phone_number  | string     | null: false                    | //電話番号
