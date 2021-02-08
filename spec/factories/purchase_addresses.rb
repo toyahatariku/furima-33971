e57a88496b2412aa8b973bbe085f1c9f323707e6
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-08 14:01:59 +0900

    FactoryBotの修正

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,13 +1,15 @@
 FactoryBot.define do
   factory :purchase_address do
-    purchase          { 546 }
+    purchase_id       { 546 }
     postal_code       {"634-3810"}
     prefecture_id     {22}
     municipality      {"宮本町"}
     address           {"634番地"}
     building_name     {"スカイツリー"}
     phone_number      {"09012345678"}
+    token             {"tok_abcdefghijk00000000000000000"}
 
-    association :purchase
+    user_id           {546}
+    item_id           {11}
   end
 end
