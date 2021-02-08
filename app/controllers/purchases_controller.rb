04afdab89a05f24cdaddeb0fb75c364ddba8dbb7
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-08 16:04:22 +0900

    出品者以外のユーザーは購入ページから各位ページへ遷移する機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,15 +2,21 @@ class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
+      if !user_signed_in? #ログインしていないとログインページへ遷移する
+        redirect_to new_user_session_path 
+      elsif  current_user.id == @item.user_id #ログインユーザーが出品者と同じならトップページへ遷移する
+        redirect_to root_path 
+      elsif nil != (Purchase.find_by(item_id: @item.id))  #商品が購入済みならトップページへ遷移する
+        redirect_to root_path 
+      end
   end
-
   def create
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchases_params)
     if @purchase_address.valid?
       @purchase_address.save
       pay_item
-      redirect_to item_path(@item.id)
+      redirect_to root_path 
     else
       render 'index'
     end
@@ -30,5 +36,9 @@ class PurchasesController < ApplicationController
         currency:'jpy'
       )
    end
+
+  def back_index
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
 
 end
