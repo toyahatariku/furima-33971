8124422f6e1adc2f4fd9fd0a9c72441891e266b8
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-08 12:12:59 +0900

    FactoryBotのモデル変更

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :purchase_address do
-    
+    purchase          { 546 }
+    postal_code       {"634-3810"}
+    prefecture_id     {22}
+    municipality      {"宮本町"}
+    address           {"634番地"}
+    building_name     {"スカイツリー"}
+    phone_number      {"09012345678"}
+
+    association :purchase
   end
 end
