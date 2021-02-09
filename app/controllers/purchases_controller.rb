83a09a908f2eb9f7cde31913844b6a8e56822c20
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-09 11:20:00 +0900

    非ログインの対処をif条件からbefore_action :authenticate_user!へ変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,10 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :edit]
   before_action :item_param, only: [:index, :create]
 
   def index
     @purchase_address = PurchaseAddress.new
-    if !user_signed_in? # ログインしていないとログインページへ遷移する
-      redirect_to new_user_session_path
-    elsif current_user.id == @item.user_id # ログインユーザーが出品者と同じならトップページへ遷移する
+    if current_user.id == @item.user_id # ログインユーザーが出品者と同じならトップページへ遷移する
       redirect_to root_path
     elsif nil != (Purchase.find_by(item_id: @item.id))  # 商品が購入済みならトップページへ遷移する
       redirect_to root_path
