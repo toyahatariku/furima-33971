9dc9712af2905ed440037be9f9174b1eeb35951f
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-09 11:23:44 +0900

    ログインと出品者が同じ場合の記述をbefore_action :back_indexへ変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,11 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :edit]
   before_action :item_param, only: [:index, :create]
+  before_action :back_index, only: [:index]
 
   def index
     @purchase_address = PurchaseAddress.new
-    if current_user.id == @item.user_id # ログインユーザーが出品者と同じならトップページへ遷移する
-      redirect_to root_path
-    elsif nil != (Purchase.find_by(item_id: @item.id))  # 商品が購入済みならトップページへ遷移する
+    if nil != (Purchase.find_by(item_id: @item.id))  # 商品が購入済みならトップページへ遷移する
       redirect_to root_path
     end
   end
