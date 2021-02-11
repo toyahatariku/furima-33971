0741fb783c4140c89011a208a026a41919151663
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-11 16:44:08 +0900

    シークレット認証を環境変数に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'kuritahayato' && password == '123456'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
