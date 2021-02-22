aeb4095a72461a5e93b270650b1df9808d79b4ca
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-02-22 18:22:29 +0900

    purchase_addressのテストコードの修正

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -28,87 +28,87 @@ RSpec.describe PurchaseAddress, type: :model do
       it '郵便番号が空だと記録されない' do
         @purchase_address.postal_code = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "Postal code can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "郵便番号を入力してください"
       end
       it '郵便番号が7桁でないと記録されない' do
         @purchase_address.postal_code = '012-345'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Postal code 郵便番号は「-」を含む且つ7桁'
+        expect(@purchase_address.errors.full_messages).to include '郵便番号「-」を含む且つ7桁で入力して下さい'
       end
       it '郵便番号にハイフンが入っていないと記録されない' do
         @purchase_address.postal_code = '0123456'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Postal code 郵便番号は「-」を含む且つ7桁'
+        expect(@purchase_address.errors.full_messages).to include '郵便番号「-」を含む且つ7桁で入力して下さい'
       end
 
       it '都道府県が空だと記録されない' do
         @purchase_address.prefecture_id = nil
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "Prefecture can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "発送元の地域を入力してください"
       end
       it '都道府県の選択が1「---」だと記録されない' do
         @purchase_address.prefecture_id = 1
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Prefecture 都道府県を選んで下さい'
+        expect(@purchase_address.errors.full_messages).to include '発送元の地域は1以外の値にしてください'
       end
 
       it '市区町村が空だと記録されない' do
         @purchase_address.municipality = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "Municipality can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "市区町村を入力してください"
       end
 
       it '番地が空だと記録されない' do
         @purchase_address.address = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "Address can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "番地を入力してください"
       end
 
       it '電話番号が空だと記録されない' do
         @purchase_address.phone_number = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "Phone number can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "電話番号を入力してください"
       end
       it '電話番号が全角数字だと記録されない' do
         @purchase_address.phone_number = '０９０１２３４５６７８'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Phone number 電話番号は「-」無しの半角10桁か11桁'
+        expect(@purchase_address.errors.full_messages).to include '電話番号「-」無しの半角10桁か11桁で入力して下さい'
       end
       it '電話番号が英数字混合だと記録されない' do
         @purchase_address.phone_number = '0901234abcd'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Phone number 電話番号は「-」無しの半角10桁か11桁'
+        expect(@purchase_address.errors.full_messages).to include '電話番号「-」無しの半角10桁か11桁で入力して下さい'
       end
       it '電話番号にハイフンが入っていると記録されない' do
         @purchase_address.phone_number = '090-1234567'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Phone number 電話番号は「-」無しの半角10桁か11桁'
+        expect(@purchase_address.errors.full_messages).to include '電話番号「-」無しの半角10桁か11桁で入力して下さい'
       end
       it '電話番号が12桁以上だと記録されない' do
         @purchase_address.phone_number = '090123456789'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Phone number 電話番号は「-」無しの半角10桁か11桁'
+        expect(@purchase_address.errors.full_messages).to include '電話番号「-」無しの半角10桁か11桁で入力して下さい'
       end
       it '電話番号が9桁以下だと記録されない' do
         @purchase_address.phone_number = '090123456'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include 'Phone number 電話番号は「-」無しの半角10桁か11桁'
+        expect(@purchase_address.errors.full_messages).to include '電話番号「-」無しの半角10桁か11桁で入力して下さい'
       end
 
       it 'トークンが空だと記録されない' do
         @purchase_address.token = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "Token can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "クレジットカード情報を入力してください"
       end
       it 'user_idが無いと記録されない' do
         @purchase_address.user_id = nil
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "User can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "Userを入力してください"
       end
       it 'item_idが無いと記録されない' do
         @purchase_address.item_id = nil
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include "Item can't be blank"
+        expect(@purchase_address.errors.full_messages).to include "Itemを入力してください"
       end
     end
   end
