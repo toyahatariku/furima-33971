2201fc7b6bfb81e4caa477dbc8455e1ab1a7afde
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-03-04 15:44:51 +0900

    商品出品時のタグ逐次検索機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,11 +13,11 @@ class ItemsController < ApplicationController
   end
   
   def search
-    return nil if params [:keyword] == ""
-    tag = Tag.where(['name LIKE ?', "%#{params[:keyword]}%"])
+    return nil if params[:keyword] == ""
+    tag = Tag.where(['tag LIKE ?', "%#{params[:keyword]}%"] )
     render json:{ keyword: tag }
   end
-  
+
   def create
     @items_tag = ItemsTag.new(item_params)
     if @items_tag.valid? 
